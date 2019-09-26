af495bc563fc3998321d1287bb81fa7d4fe28a21
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-09-26 17:10:07 +0300

    Fix linter

--- webpack.config.js
+++ webpack.config.js
@@ -4,61 +4,61 @@ const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const PATHS = {
-    source: path.join(__dirname, 'src'),
-    public: path.join(__dirname, 'public'),
+  source: path.join(__dirname, 'src'),
+  public: path.join(__dirname, 'public'),
 };
 
 module.exports = {
-    entry: path.join(PATHS.source, 'index.js'),
+  entry: path.join(PATHS.source, 'index.js'),
 
-    output: {
-        path: path.join(PATHS.public),
-        filename: 'bundle.js'
-    },
+  output: {
+    path: path.join(PATHS.public),
+    filename: 'bundle.js',
+  },
 
-    mode: NODE_ENV === 'development' ? 'development' : 'production',
+  mode: NODE_ENV === 'development' ? 'development' : 'production',
 
-    devtool: NODE_ENV === 'development' ? 'cheap-inline-module-source-map' : false,
+  devtool: NODE_ENV === 'development' ? 'cheap-inline-module-source-map' : false,
 
-    plugins: [
-        new webpack.NoEmitOnErrorsPlugin(),
-        new webpack.DefinePlugin({
-            NODE_ENV: JSON.stringify(NODE_ENV)
-        }),
-        new webpack.NamedModulesPlugin()
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify(NODE_ENV),
+    }),
+    new webpack.NamedModulesPlugin(),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: PATHS.source,
+        loader: 'babel-loader',
+        sideEffects: false,
+      },
     ],
-
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                include: PATHS.source,
-                loader: 'babel-loader',
-                sideEffects: false
-            }
-        ]
-    }
+  },
 };
 
 if (NODE_ENV === 'production') {
-    module.exports.plugins.push(
-        new UglifyJsPlugin({
-            uglifyOptions: {
-                sourceMap: false,
-                beautify: false,
-                comments: false,
-                mangle: {
-                    keep_fnames: true
-                },
-                compress: {
-                    sequences: true,
-                    booleans: true,
-                    loops: true,
-                    unused: true,
-                    warnings: false,
-                    drop_console: true
-                }
-            }
-        })
-    );
+  module.exports.plugins.push(
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        sourceMap: false,
+        beautify: false,
+        comments: false,
+        mangle: {
+          keep_fnames: true,
+        },
+        compress: {
+          sequences: true,
+          booleans: true,
+          loops: true,
+          unused: true,
+          warnings: false,
+          drop_console: true,
+        },
+      },
+    }),
+  );
 }
